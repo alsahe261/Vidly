3f4134cd114629187df39419bf8d946770a9d87f
Alexander Salguero <alsahe26@gmail.com>
2018-05-11 01:55:47 -0500

    Section 2: Ejercicio propuesto. Implementacion Customers

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith" },
+                new Customer { Id = 2, Name = "Mary Williams" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = null,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        [Route("Customers/Details/{id}")]
+        public ActionResult Details (int id)
+        {
+            return Content(Convert.ToString(id));
+        }
+    }
+}
