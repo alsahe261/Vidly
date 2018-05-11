049fb2e4b97b00a84bd93776279cccc1b537350d
Alexander Salguero <asalguero@imbanaco.com.co>
2018-05-11 14:23:49 -0500

    Se mejora maestro detalle de Customers

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -8,30 +8,41 @@ using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
-    public class CustomersController : Controller
+  public class CustomersController : Controller
+  {
+    // GET: Customers
+    public ActionResult Index()
     {
-        // GET: Customers
-        public ActionResult Index()
-        {
-            var customers = new List<Customer>
-            {
-                new Customer { Id = 1, Name = "John Smith" },
-                new Customer { Id = 2, Name = "Mary Williams" }
-            };
+      var customers = new List<Customer>
+      {
+        new Customer { Id = 1, Name = "John Smith" },
+        new Customer { Id = 2, Name = "Mary Williams" }
+      };
 
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = null,
-                Customers = customers
-            };
+      var viewModel = new RandomMovieViewModel
+      {
+        Movie = null,
+        Customers = customers
+      };
 
-            return View(viewModel);
-        }
-
-        [Route("Customers/Details/{id}")]
-        public ActionResult Details (int id)
-        {
-            return Content(Convert.ToString(id));
-        }
+      return View(viewModel);
     }
+
+    [Route("Customers/Details/{id}")]
+    public ActionResult Details(int id)
+    {
+      var customers = new List<Customer>
+      {
+        new Customer { Id = 1, Name = "John Smith" },
+        new Customer { Id = 2, Name = "Mary Williams" }
+      };
+
+      var customer = customers.Where(cust => cust.Id == id).SingleOrDefault();
+
+      if (customer == null)
+        return HttpNotFound();
+
+      return View(customer);
+    }
+  }
 }
